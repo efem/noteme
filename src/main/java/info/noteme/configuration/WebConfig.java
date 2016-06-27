80431e1a6809d4f2fe64386febd63484b466c715
efem-pc <qlkid@o2.pl>
2016-06-28 01:30:27 +0200

    FTL OK

--- src/main/java/info/noteme/configuration/WebConfig.java
+++ src/main/java/info/noteme/configuration/WebConfig.java
@@ -1,5 +1,6 @@
 package info.noteme.configuration;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
@@ -8,12 +9,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
 import org.springframework.web.servlet.view.freemarker.FreeMarkerViewResolver;
 
+import freemarker.template.TemplateException;
 import freemarker.template.utility.XmlEscape;
 
 @Configuration
@@ -26,13 +29,14 @@ public class WebConfig extends WebMvcConfigurerAdapter {
 	 * resolver.setPrefix("/WEB-INF/views/ftl/"); resolver.setSuffix(".ftl");
 	 * resolver.setExposeContextBeansAsAttributes(true); return resolver; }
 	 */
-
+	
 	@Bean
 	public FreeMarkerViewResolver freemarkerViewResolver() {
 		FreeMarkerViewResolver resolver = new FreeMarkerViewResolver();
 		resolver.setOrder(1);
-		// resolver.setCache(true);
-		resolver.setPrefix("/WEB-INF/views/ftl/");
+		resolver.setCache(false);
+		//resolver.setPrefix("/WEB-INF/views/ftl/");
+		resolver.setPrefix("");
 		resolver.setSuffix(".ftl");
 		return resolver;
 	}
@@ -46,19 +50,19 @@ public class WebConfig extends WebMvcConfigurerAdapter {
 	}
 
 	@Bean
-	public FreeMarkerConfigurer freemarkerConfig() {
+	public FreeMarkerConfigurer freemarkerConfig(WebApplicationContext applicationContext) throws IOException, TemplateException {
 		FreeMarkerConfigurer config = new FreeMarkerConfigurer();
-		Properties property = new Properties();
-		property.setProperty("default_encoding", "UTF-8");
-
 		Map<String, Object> variables = new HashMap<String, Object>();
+		Properties property = new Properties();
+		
+		property.setProperty("default_encoding", "UTF-8");
 		variables.put("xml_escape", fmXmlEscape);
+		config.setServletContext(applicationContext.getServletContext());
 		config.setFreemarkerVariables(variables);
-		// config.setTemplateLoaderPath("/WEB-INF/views/ftl");
+		 config.setTemplateLoaderPath("/WEB-INF/views/ftl");
 		config.setFreemarkerSettings(property);
 		return config;
 	}
-
 	
 	@Override
 	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {

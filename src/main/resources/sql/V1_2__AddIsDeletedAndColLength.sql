3597c35107d68a2a3ed87b34743b2ddb3864b2df
efem-pc <qlkid@o2.pl>
2016-07-10 23:30:19 +0200

    default not null iddeleted flyway

--- src/main/resources/sql/V1_2__AddIsDeletedAndColLength.sql
+++ src/main/resources/sql/V1_2__AddIsDeletedAndColLength.sql
@@ -1,3 +1,3 @@
 ALTER TABLE users CHANGE COLUMN password password VARCHAR(60) NOT NULL ;
 
-ALTER TABLE users ADD COLUMN isDeleted BIT(1) NOT NULL AFTER password;
+ALTER TABLE users ADD COLUMN isDeleted BIT(1) NOT NULL DEFAULT 0 AFTER password;

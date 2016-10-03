1847722c979ef2b856f0368fc3a147710661ed6c
efem <qlkid@o2.pl>
2016-10-03 17:55:03 +0200

    REST ajax

--- src/main/webapp/resources/js/note-js.js
+++ src/main/webapp/resources/js/note-js.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 	$('#randomPerson').click(function() {
-        $.getJSON('showOne', function(person) {
+        $.getJSON('${request.requestObject.contextPath} ', function(person) {
           $('#personResponse').text(note.content + ', id ' + note.id);
         });
       });

809f22c42815980ceb735d9081d49c06697e8da8
efem <qlkid@o2.pl>
2016-10-07 22:50:08 +0200

    Flyway maintenance

--- src/main/resources/sql/V1_6__Modify_Table_Note_Add_Dates.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE note ADD COLUMN dateadded datetime DEFAULT NULL AFTER trynick;
-ALTER TABLE note ADD COLUMN datemodified datetime DEFAULT NULL AFTER dateadded;
-ALTER TABLE note ADD COLUMN mailtosend BIT(1) NOT NULL DEFAULT 0 AFTER datemodified;
-ALTER TABLE note ADD COLUMN wasmailsend BIT(1) NOT NULL DEFAULT 0 AFTER mailtosend;
-ALTER TABLE note ADD COLUMN nickfound BIT(1) NOT NULL DEFAULT 0 AFTER wasmailsend;
---
+++ src/main/resources/sql/V1_6__Note_Populate.sql
@@ -0,0 +1,12 @@
+INSERT INTO note
+(content, nickfound, trynick, dateadded, datemodified, mailtosend, wasmailsend, userid)
+VALUES
+("This is very first note", 0, "This", "2016-10-06 05:05:05", "2016-10-06 05:05:05", 0, 0, null);
+INSERT INTO note
+(content, nickfound, trynick, dateadded, datemodified, mailtosend, wasmailsend, userid)
+VALUES
+("Second note a bit longet than the first one but this within boundaries", 0, "Second", "2016-10-06 06:06:06", "2016-10-06 06:06:06", 0, 0, null);
+INSERT INTO note
+(content, nickfound, trynick, dateadded, datemodified, mailtosend, wasmailsend, userid)
+VALUES
+("This note has an author", 1, "This", "2016-10-06 05:05:05", "2016-10-06 05:05:05", 0, 0, 1);
